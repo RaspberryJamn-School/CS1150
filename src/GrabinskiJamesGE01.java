33054636ca78d4ab20c2e5b216bb08a747696865
RaspberryJamn-School <111931148+RaspberryJamn-School@users.noreply.github.com>
2022-08-23 18:26:18 -0600

    Update GrabinskiJamesGE01.java

--- src/GrabinskiJamesGE01.java
+++ src/GrabinskiJamesGE01.java
@@ -1,9 +1,21 @@
 
 public class GrabinskiJamesGE01 {
-
+	private static int raindrop_number(int arg) {
+		System.out.print(arg);
+		System.out.print(" ");
+		if (arg == 1) {
+			return 1;
+		}
+		if (arg % 2 == 0) {
+			return raindrop_number(arg/2);
+		} else {
+			return raindrop_number(arg*3+1);
+		}
+		
+	}
 	public static void main(String[] args) {
 		System.out.println("Oi oi oi we coding");
-
+		raindrop_number(6);
 	}
 
 }
