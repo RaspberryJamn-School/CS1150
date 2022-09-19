84148a1345bf0595befe2afcd91d60dd280d651a
RaspberryJamn-School <111931148+RaspberryJamn-School@users.noreply.github.com>
2022-09-19 11:20:57 -0600

    stick to the rules

--- src/GrabinskiJamesValidateStrictRules.java
+++ src/GrabinskiJamesValidateStrictRules.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class GrabinskiJamesValidateCreditFormat {
+public class GrabinskiJamesValidateStrictRules {
 
 	public static boolean areLetters(String testString) { // test if all chars in a string are letters
 		for (int i = 0; i < testString.length(); i++) {
@@ -10,7 +10,7 @@ public class GrabinskiJamesValidateCreditFormat {
 		}
 		return true; // if we never found a non letter char, pass
 	}
-	
+
 	public static boolean areDigits(String testString) {
 		for (int i = 0; i < testString.length(); i++) {
 			if (!Character.isDigit(testString.charAt(i))) {
@@ -20,42 +20,36 @@ public class GrabinskiJamesValidateCreditFormat {
 		return true;
 	}
 	
-	public static boolean evalCreditCardValidity(String cardString) {
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter a card of the format XXXX-0000-0000: ");
+		String cardString = input.nextLine().trim();
+		input.close();
 		if (cardString.length()!=14) {
 			System.out.println("Fail, card length was incorrect");
-			return false;
+			System.exit(0);
 		}
-		if (!areLetters(cardString.substring(0, 4))) {
+		if (!areLetters(cardString.substring(0, 4))) { // extract first 4 chars from the input string
 			System.out.println("Fail, first block is not alphabetic");
-			return false;
-		}
-		if (cardString.charAt(4)!='-') {
-			System.out.println("Fail, card string separators must be '-'");
-			return false;
+			System.exit(0);
 		}
 		if (!areDigits(cardString.substring(5, 9))) {
 			System.out.println("Fail, second block is not numeric");
-			return false;
-		}
-		if (cardString.charAt(9)!='-') {
-			System.out.println("Fail, card string separators must be '-'");
-			return false;
+			System.exit(0);
 		}
 		if (!areDigits(cardString.substring(10, 14))) {
 			System.out.println("Fail, third block is not numeric");
-			return false;
+			System.exit(0);
 		}
-		System.out.println("Success! Card string valid");
-		return true;
-	}
-	
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.println("Please enter a card of the format XXXX-0000-0000: ");
-		String fullInput = input.nextLine().trim();
-		boolean isCreditCardValid = evalCreditCardValidity(fullInput);
-		System.out.println(isCreditCardValid);
-		input.close();
-	}
+		if (cardString.charAt(4)!='-') {
+			System.out.println("Fail, card string separators must be '-'");
+			System.exit(0);
+		}
+		if (cardString.charAt(9)!='-') {
+			System.out.println("Fail, card string separators must be '-'");
+			System.exit(0);
+		}
 
+		System.out.println("Correct length 14 (you did it!)");
+	}
 }
