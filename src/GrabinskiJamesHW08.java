8405672e1a8a8a86d9d05b94570f397c6a76cb1f
RaspberryJamn <81341917+RaspberryJamn@users.noreply.github.com>
2022-11-06 17:16:01 -0700

    Create GrabinskiJamesHW08.java

---
+++ src/GrabinskiJamesHW08.java
@@ -0,0 +1,47 @@
+
+public class GrabinskiJamesHW08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class Student {
+	String name;
+	double gpa;
+	
+	public Student(String name, double gpa) {
+		this.name = name;
+		this.gpa = gpa;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public double getGPA() {
+		return this.gpa;
+	}
+	
+	public void setGPA(double gpa) {
+		this.gpa = gpa;
+	}
+}
+
+class Course {
+	String name;
+	int maxStudents;
+	Student[] students;
+	int numStudents;
+	static int numCourses = 0;
+	
+	public Course(String name, int maxNumStudents) {
+		this.name = name;
+		this.maxStudents = maxNumStudents;
+		students = new Student[maxNumStudents];
+		numStudents = 0;
+	}
+	
+}
