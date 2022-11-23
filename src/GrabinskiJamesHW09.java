55ef91549ca7303a537975c8aff03579475a32d9
RaspberryJamn-School <111931148+RaspberryJamn-School@users.noreply.github.com>
2022-11-22 19:02:17 -0700

    Update GrabinskiJamesHW09.java

--- src/GrabinskiJamesHW09.java
+++ src/GrabinskiJamesHW09.java
@@ -3,11 +3,39 @@ public class GrabinskiJamesHW09 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Car car = new Car("James", "Minecraft", Double.MAX_VALUE, Double.MAX_VALUE);
-		System.out.println(car.getFuelGauge().getGallons());
+//		Car car = new Car("James", "Minecraft", Double.MAX_VALUE, Double.MAX_VALUE);
+//		System.out.println(car.getFuelGauge().getGallons());
+//		
+		Car[] cars = new Car[5];
+		createCars(cars);
+		System.out.println("Cars at start of simulation");
+		printCars(cars);
 	}
-
-}
+	
+	private static void createCars(Car[] cars) {
+		cars[0] = new Car("Shrek", "Toyota Tundra", 15, 6);
+		cars[1] = new Car("Fiona", "Audi Q7", 21, 10);
+		cars[2] = new Car("Donkey", "Jeep CJ5", 14, 5);
+		cars[3] = new Car("Farquaad", "Smart Car", 42, 4);
+		cars[4] = new Car("Dragon", "Chevy Suburban", 12, 30);
+	}
+	
+	private static String padWidthByTab(String base, int tabWidth) {
+		String ret = base;
+		for (int i = 0; i < Math.ceil((tabWidth*8-base.length())/8.0); i++) {
+			ret += "	";
+		}
+		return ret;
+	}
+	private static void printCars(Car[] cars) {
+		System.out.println("-----------------------------------------------------------------------");
+		System.out.println("Owner		Brand			Fuel Economy	Current Gallons");
+		System.out.println("-----------------------------------------------------------------------");
+		for (int i = 0; i < cars.length; i++) {
+			System.out.printf("%s%s%.2f		%.2f\n", padWidthByTab(cars[i].getOwner(), 2), padWidthByTab(cars[i].getBrand(), 3), cars[i].getFuelEconomy(), cars[i].getFuelGauge().getGallons());
+		}
+	}
+} 
 
 /**Class that represents one car
  * */
