9a3d44b84f32a86ccf5ba5e92318d85e5da9d596
RaspberryJamn <81341917+RaspberryJamn@users.noreply.github.com>
2022-11-27 23:45:53 -0700

    write to file & comments

--- src/GrabinskiJamesHW09.java
+++ src/GrabinskiJamesHW09.java
@@ -1,6 +1,6 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class GrabinskiJamesHW09 {
 
@@ -35,6 +35,12 @@ public class GrabinskiJamesHW09 {
 		Car shortestRunCar = findCarWithNoGas(cars);
 		System.out.println(shortestRunCar.getOwner() + "'s car ran out of gas after " + milesTraveled + " miles");
 		
+		try {
+			writeCarDetailsToFile(cars);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 	}
 	
 	private static void createCars(Car[] cars) {
@@ -45,7 +51,7 @@ public class GrabinskiJamesHW09 {
 		cars[4] = new Car("Dragon", "Chevy Suburban", 12, 30);
 	}
 	
-	private static String padWidthByTab(String base, int tabWidth) {
+	private static String padWidthByTab(String base, int tabWidth) { // attempts to make [base] take up [tabWidth]-tabs worth of space; if the string is too long, it wont add anything
 		String ret = base;
 		for (int i = 0; i < Math.ceil((tabWidth*8-base.length())/8.0); i++) {
 			ret += "	";
@@ -73,12 +79,26 @@ public class GrabinskiJamesHW09 {
     }
     
     public static void writeCarDetailsToFile(Car[] cars) throws IOException {
-    	File file = new File("hw09output.txt");
-    	Scanner writefile = new Scanner(file);
-    	for (int i = 0; i < cars.length; i++) {
-			System.out.printf("%s%s%.2f		%.2f\n", padWidthByTab(cars[i].getOwner(), 2), padWidthByTab(cars[i].getBrand(), 3), cars[i].getFuelEconomy(), cars[i].getFuelGauge().getGallons());
-		}
-    	writefile.close();
+		final String FILENAME = "Assignment11.txt";
+		try {
+			File file = new File(FILENAME);
+			file.createNewFile();
+//			if (file.createNewFile()) {
+//				System.out.println("File created: " + file.getName());
+//			} else {
+//				System.out.println("File already exists.");
+//			}
+//			System.out.println("File is located at: " + file.getAbsolutePath());
+			
+			FileWriter fileWriter = new FileWriter(FILENAME);
+			for (int i = 0; i < cars.length; i++) {
+				fileWriter.write(String.format("%s%s%.2f		%.2f\n", padWidthByTab(cars[i].getOwner(), 2), padWidthByTab(cars[i].getBrand(), 3), cars[i].getFuelEconomy(), cars[i].getFuelGauge().getGallons()));
+			}
+			fileWriter.close();
+	    } catch (IOException e) {
+	    	System.out.println("An error occurred.");
+	    	e.printStackTrace();
+	    }
     }
 }
 
@@ -98,6 +118,8 @@ class Car {
 		
 	}
 	
+	/**decrements the gallons within the Car's fuelGauge object according the Car's fuel economy and the distance traveled
+	 * */
 	public void updateFuel(double milesTraveled) {
 		double proposedFuelUsage = milesTraveled/fuelEconomy;
 		fuelGauge.decrementGallons(proposedFuelUsage);
@@ -129,7 +151,8 @@ class Car {
 
 }
 
-/**Class that represents the fuel gauge in a car*/
+/**Class that represents the fuel gauge in a car; 
+ * internal gallons variable cannot be set, only initialized and decremented*/
 class FuelGauge {
 	private double gallons;
 	
@@ -141,6 +164,8 @@ class FuelGauge {
 		return gallons;
 	}
 	
+	/**decrements gallons; 
+	 * if decrementing would yield a negative value, gallons is set to zero instead*/
 	public void decrementGallons(double gallonsUsed) {
 		if (gallonsUsed > this.gallons) {
 			this.gallons = 0.0;
